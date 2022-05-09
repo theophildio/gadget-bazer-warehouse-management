e7555a8a3ef620980ed5aa2fdc96f042f3654be5
theophildio <dtheo.sangma@gmail.com>
2022-05-10 00:25:39 +0600

    Added some design to Single product

--- src/Pages/Home/FeaturedCollection/SingleProduct/SingleProduct.js
+++ src/Pages/Home/FeaturedCollection/SingleProduct/SingleProduct.js
@@ -2,19 +2,20 @@ import React from "react";
 import "./SingleProduct.css";
 
 const SingleProduct = ({ item }) => {
-	const { image, name, details, price, vendor, quantity } = item;
+	const { image, name, details, price, Vendor, Quantity } = item;
 	return (
 		<div className="single-product">
 			<div className="img">
 				<img src={image} alt="" />
 			</div>
       <div className="item-detail">
-        <h4>{name}</h4>
-				<h4>{vendor}</h4>
+        <h4 className="product-title">{name}</h4>
+				<h4>Vendor: {Vendor}</h4>
+				<h5>Quantity: {Quantity}</h5>
         <p>{details}</p>
-				<p>{quantity}</p>
-        <p>${price}</p>
+        <h5>${price}</h5>
       </div>
+			<button className="gadget-btn">Update</button>
 		</div>
 	);
 };
